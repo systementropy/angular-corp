acb58cbce9894329e59cdc3d5830d24dc70b03cd
systementropy <vatsa.saket@gmail.com>
2017-07-03 11:56:43 +0530

    parallax demo

--- app/script/test.js
+++ app/script/test.js
@@ -12,9 +12,11 @@
         }
     ])
 
-    .controller('TestCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location', function($rootScope,$scope, $http,$timeout,$location) {
+    .controller('TestCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location', 'parallaxHelper', function($rootScope,$scope, $http,$timeout,$location, parallaxHelper) {
         $scope.init = function(){
             $rootScope.hideHeader=false;
+            $scope.background = parallaxHelper.createAnimator(-0.5, 0, 100);
+            $scope.foreground = parallaxHelper.createAnimator(0.7, 3000, -1500);
         };
         
         $scope.init();
