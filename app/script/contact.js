f95b607fcd4122e263cde59f6e4c8bb8d9508b3f
systementropy <vatsa.saket@gmail.com>
2017-07-13 16:12:42 +0530

    contact map utility

--- app/script/contact.js
+++ app/script/contact.js
@@ -13,21 +13,18 @@
     .controller('ContactCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location','NgMap', function($rootScope,$scope, $http,$timeout,$location,NgMap) {
         
         $scope.init = function(){
-            // NgMap.getMap().then(function(map) {
-            //     console.log(map.getCenter());
-            //     console.log('markers', map.markers);
-            //     console.log('shapes', map.shapes);
-            // });
+            //  
             $scope.shape = {
               coords: [1, 1, 1, 20, 18, 20, 18 , 1],
               type: 'poly'
             };
             $scope.addArr =[
+                {'id':"id16",'add':"Mumbai",'type':'head'},
                 {'id':"id1",'add':"201,sarthik 2nd Ahmedabad",'type':'bureau'},
                 {'id':"id2",'add':"Millenia Tower, Tower - C, Bangalore - 560008",'type':'bureau'},
                 {'id':"id3",'add':"Mb-4, Mezzanine Mansarover Complex, Bhopal",'type':'bureau'},
                 {'id':"id4",'add':"Room no. 204, Hotel Sungreen, Orissa",'type':'bureau'},
-                {'id':"id5",'add':"House no.1284,UT Chandigarh",'type':'head'},
+                {'id':"id5",'add':"Gurgaon",'type':'head'},
                 {'id':"id6",'add':"No.2,p.s.sivaswamy salai,2nd street,mylapore,-chennai-600004",'type':'bureau'},
                 {'id':"id7",'add':"Hotel brahmputra ahsok, m.g. road, guwahati ",'type':'bureau'},
                 {'id':"id8",'add':"D/no: 6-3-902/a, 4th floor, plot no : 9, central plaza, rajbhavan road, somajiguda, hyderabad - 500082.",'type':'bureau'},
