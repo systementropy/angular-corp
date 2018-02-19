0f982fcd4efe14665134228b21d18fa71a96b369
systementropy <vatsa.saket@gmail.com>
2018-02-19 14:18:20 +0530

    text update

--- app/script/portfolio-main.js
+++ app/script/portfolio-main.js
@@ -44,36 +44,56 @@
 
 
 	    $scope.init = function(){
-	    	$scope.fadeIn = parallaxHelper.createAnimator(0.15, 0, 1,-850);
-	    	$scope.background = parallaxHelper.createAnimator(-0.1, 0, 0,-400);
-	    	$scope.background2 = parallaxHelper.createAnimator(0.1, -1000, 1000,-400);
-	    	$scope.anmtXfrmLFade = parallaxHelper.createAnimator(0.4, 0, 1, -650);
+	    	if ($(window).width()>1000) {
+	    		$scope.fadeIn = parallaxHelper.createAnimator(0.15, 0, 1,-850);
+	    		$scope.background = parallaxHelper.createAnimator(-0.1, 0, 0,-400);
+	    		$scope.background2 = parallaxHelper.createAnimator(0.1, -1000, 1000,-400);
+	    		$scope.anmtXfrmLFade = parallaxHelper.createAnimator(0.4, 0, 1, -650);
 
-	    	$scope.anmtXfrmL = parallaxHelper.createAnimator(0.4, -300, 100, -1400);
-	    	$scope.anmtXfrmL3 = parallaxHelper.createAnimator(0.4, -300, 0, -1400);
-	    	$scope.anmtXfrmR = parallaxHelper.createAnimator(0.9, 100, -278, -1100);
-	    	$scope.anmtXfrmR = parallaxHelper.createAnimator(0.9, 300, 0, -1100);
-	    	$scope.anmtXfrmLFade2 = parallaxHelper.createAnimator(0.4, 0, 1, -650);
-	    	$scope.foreground = parallaxHelper.createAnimator(0.1, 0, -1000,-400);
-	    	$scope.foregroundvf = parallaxHelper.createAnimator(0.15, 100, -1000,-400);
-	    	$scope.backgroundimg = parallaxHelper.createAnimator(-0.1, 0, 0,-400);
-	    	$scope.foregroundimg = parallaxHelper.createAnimator(0.1, 1000, -1000,-400);
-	    	$scope.foregroundimg2 = parallaxHelper.createAnimator(0.15, 1000, -1000,-400);
-	    	$scope.foregroundimg3 = parallaxHelper.createAnimator(0.18, 1000, -1000,-400);
-	    	$scope.foregroundimg4 = parallaxHelper.createAnimator(0.4, 0, -300,-700);
-	    	$scope.foregroundimgz2m2 = parallaxHelper.createAnimator(0.4, 1000, -2000,-400);
-	    	$scope.foregroundimgz2m = parallaxHelper.createAnimator(-0.05, 1000, -1000,-400);
-	    	$scope.foregroundimgssc = parallaxHelper.createAnimator(0.1, -10, 100,-400);
-	    	$scope.backgroundgroundimgm2z = parallaxHelper.createAnimator(0.05, -100, 100,-400);
-	    	$scope.foregroundimgz2m2slow = parallaxHelper.createAnimator(0.2, 0, -1000,-1000);
+	    		$scope.anmtXfrmL = parallaxHelper.createAnimator(0.4, -300, 100, -1400);
+	    		$scope.anmtXfrmL3 = parallaxHelper.createAnimator(0.4, -300, 0, -1400);
+	    		$scope.anmtXfrmR = parallaxHelper.createAnimator(0.9, 100, -278, -1100);
+	    		$scope.anmtXfrmR = parallaxHelper.createAnimator(0.9, 300, 0, -1100);
+	    		$scope.anmtXfrmLFade2 = parallaxHelper.createAnimator(0.4, 0, 1, -650);
+	    		$scope.foreground = parallaxHelper.createAnimator(0.1, 0, -1000,-400);
+	    		$scope.foregroundvf = parallaxHelper.createAnimator(0.15, 100, -1000,-400);
+	    		$scope.backgroundimg = parallaxHelper.createAnimator(-0.1, 0, 0,-400);
+	    		$scope.foregroundimg = parallaxHelper.createAnimator(0.1, 1000, -1000,-400);
+	    		$scope.foregroundimg2 = parallaxHelper.createAnimator(0.15, 1000, -1000,-400);
+	    		$scope.foregroundimg3 = parallaxHelper.createAnimator(0.18, 1000, -1000,-400);
+	    		$scope.foregroundimg4 = parallaxHelper.createAnimator(0.4, 0, -300,-700);
+	    		$scope.foregroundimgz2m2 = parallaxHelper.createAnimator(0.4, 1000, -2000,-400);
+	    		$scope.foregroundimgz2m = parallaxHelper.createAnimator(-0.05, 1000, -1000,-400);
+	    		$scope.foregroundimgssc = parallaxHelper.createAnimator(0.1, -10, 100,-400);
+	    		$scope.backgroundgroundimgm2z = parallaxHelper.createAnimator(0.05, -100, 100,-400);
+	    		$scope.foregroundimgz2m2slow = parallaxHelper.createAnimator(0.2, 0, -1000,-1000);	
+	    	}else {
+	    		$scope.fadeIn = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.background = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.background2 = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.anmtXfrmLFade = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.anmtXfrmL = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.anmtXfrmL3 = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.anmtXfrmR = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.anmtXfrmR = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.anmtXfrmLFade2 = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foreground = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundvf = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.backgroundimg = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimg = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimg2 = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimg3 = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimg4 = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimgz2m2 = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimgz2m = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimgssc = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.backgroundgroundimgm2z = parallaxHelper.createAnimator(0,0,0,0);
+	    		$scope.foregroundimgz2m2slow = parallaxHelper.createAnimator(0,0,0,0);
+	    	}
+			
 	    	var show_elt = $location.absUrl().split('#')[1];
 	    	$scope.showNews = false;
-	    	if ($(window).width()>1000) {
-	    		console.log('HERE');
-	    		$scope.duParallaxDesk='du-parallax';
-	    	}else{
-	    		$scope.duParallaxDesk = '';
-	    	}
+	    	
 	    	if (show_elt == 'portfolioNews') {
 	    		$scope.showNews = true;
 	    		$scope.showElt = 'News';
