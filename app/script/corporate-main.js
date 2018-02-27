610d79c76091cd0e8de2e8019fb2461c60b2d099
systementropy <vatsa.saket@gmail.com>
2018-02-27 17:48:31 +0530

    basic timline slider

--- app/script/corporate-main.js
+++ app/script/corporate-main.js
@@ -15,6 +15,32 @@
     .controller('CorporateMainCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location','parallaxHelper', function($rootScope,$scope, $http,$timeout,$location,parallaxHelper) {
 
         $scope.init = function(){
+            $scope.slickConfig = {
+                centerMode: true,
+                slidesToShow: 1,
+                dots: true,
+                autoPlay:true,
+                responsive: [
+                    {
+                      breakpoint: 768,
+                      settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                      }
+                    },
+                    {
+                      breakpoint: 480,
+                      settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                      }
+                    }
+                ]
+            };
             $scope.fadeIn = parallaxHelper.createAnimator(0.15, 0, 1,-850);
             $scope.transY = parallaxHelper.createAnimator(0.4, 10, -10,-950);
             $scope.transYL = parallaxHelper.createAnimator(0.4, 20, -10,-850);
