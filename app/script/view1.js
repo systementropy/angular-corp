56f629d8efea7316f42322beb16ec37d92fbb5d3
systementropy <vatsa.saket@gmail.com>
2018-04-05 09:46:45 +0530

    INvestor changes

--- app/script/view1.js
+++ app/script/view1.js
@@ -14,11 +14,22 @@
 
     .controller('InvestorCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location', function($rootScope,$scope, $http,$timeout,$location) {
         $scope.changeFoc = function(num){
-            console.log(num);
             $scope.left_foc=num;
+        };
+        $scope.changeFoc1 = function(num){
+            $scope.left_foc1=num;
+        };
+        $scope.changeFoc3 = function(num){
+            $scope.left_foc3=num;
+        };
+        $scope.changeFoc2 = function(num){
+            $scope.left_foc2=num;
         };
         var init = function(){
             $scope.left_foc=0;
+            $scope.left_foc1=0;
+            $scope.left_foc3=0;
+            $scope.left_foc2=0;
             $rootScope.hideHeader=false;
         };
         init();
