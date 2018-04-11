82bf9d5eadbfd049ce9db78d3c373deb414d490b
systementropy <vatsa.saket@gmail.com>
2018-04-11 12:02:06 +0530

    tv18 info added in investor s3ection

--- app/script/view1.js
+++ app/script/view1.js
@@ -25,12 +25,30 @@
         $scope.changeFoc2 = function(num){
             $scope.left_foc2=num;
         };
+        $scope.changeFocTV0 = function(num){
+            $scope.left_foctv0=num;
+        };
+        $scope.changeFocTV1 = function(num){
+            $scope.left_foctv1=num;
+        };
+        $scope.changeFocTV2 = function(num){
+            $scope.left_foctv2=num;
+        };
+        $scope.changeFocTV3 = function(num){
+            $scope.left_foctv3=num;
+        };
         var init = function(){
             $scope.left_foc=0;
             $scope.left_foc1=0;
             $scope.left_foc3=0;
             $scope.left_foc2=0;
+            $scope.left_foctv0=0;
+            $scope.left_foctv1=0;
+            $scope.left_foctv2=0;
+            $scope.left_foctv3=0;
             $rootScope.hideHeader=false;
+            $scope.activeSec={};
+            $scope.activeSec.nw18Section=true;
         };
         init();
     }]);
