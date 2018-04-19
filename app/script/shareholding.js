baaa0600133c785d6a69bbb7ba1c6c1f8d7151b6
systementropy <vatsa.saket@gmail.com>
2018-04-19 10:01:24 +0530

    Finance: pages moved

---
+++ app/script/shareholding.js
@@ -0,0 +1,55 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp.shareholding', ['ngRoute'])
+
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when('/shareholding', {
+                templateUrl: '/view/shareholding.html',
+                controller: 'shareholdingCtrl'
+            });
+        }
+    ])
+
+    .controller('shareholdingCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location', function($rootScope,$scope, $http,$timeout,$location) {
+        $scope.changeFoc = function(num){
+            $scope.left_foc=num;
+        };
+        $scope.changeFoc1 = function(num){
+            $scope.left_foc1=num;
+        };
+        $scope.changeFoc3 = function(num){
+            $scope.left_foc3=num;
+        };
+        $scope.changeFoc2 = function(num){
+            $scope.left_foc2=num;
+        };
+        $scope.changeFocTV0 = function(num){
+            $scope.left_foctv0=num;
+        };
+        $scope.changeFocTV1 = function(num){
+            $scope.left_foctv1=num;
+        };
+        $scope.changeFocTV2 = function(num){
+            $scope.left_foctv2=num;
+        };
+        $scope.changeFocTV3 = function(num){
+            $scope.left_foctv3=num;
+        };
+        var init = function(){
+            $scope.left_foc=0;
+            $scope.left_foc1=0;
+            $scope.left_foc3=0;
+            $scope.left_foc2=0;
+            $scope.left_foctv0=0;
+            $scope.left_foctv1=0;
+            $scope.left_foctv2=0;
+            $scope.left_foctv3=0;
+            $rootScope.hideHeader=false;
+            $scope.activeSec={};
+            $scope.activeSec.nw18Section=true;
+        };
+        init();
+    }]);
+}());
