813475405e2645f5df514c26115201f6406ed59b
systementropy <vatsa.saket@gmail.com>
2018-04-19 11:25:21 +0530

    Finance-Subsidiary: files added

---
+++ app/script/finSubs.js
@@ -0,0 +1,55 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp.finSubs', ['ngRoute'])
+
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when('/finance-subsidiary', {
+                templateUrl: '/view/finSubs.html',
+                controller: 'finSubsCtrl'
+            });
+        }
+    ])
+
+    .controller('finSubsCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location', function($rootScope,$scope, $http,$timeout,$location) {
+        $scope.changeFoc = function(num){
+            $scope.left_foc=num;
+        };
+        $scope.changeFoc1 = function(num){
+            $scope.left_foc1=num;
+        };
+        $scope.changeFoc3 = function(num){
+            $scope.left_foc3=num;
+        };
+        $scope.changeFoc2 = function(num){
+            $scope.left_foc2=num;
+        };
+        $scope.changeFocTV0 = function(num){
+            $scope.left_foctv0=num;
+        };
+        $scope.changeFocTV1 = function(num){
+            $scope.left_foctv1=num;
+        };
+        $scope.changeFocTV2 = function(num){
+            $scope.left_foctv2=num;
+        };
+        $scope.changeFocTV3 = function(num){
+            $scope.left_foctv3=num;
+        };
+        var init = function(){
+            $scope.left_foc=0;
+            $scope.left_foc1=0;
+            $scope.left_foc3=0;
+            $scope.left_foc2=0;
+            $scope.left_foctv0=0;
+            $scope.left_foctv1=0;
+            $scope.left_foctv2=0;
+            $scope.left_foctv3=0;
+            $rootScope.hideHeader=false;
+            $scope.activeSec={};
+            $scope.activeSec.nw18Section=true;
+        };
+        init();
+    }]);
+}());
