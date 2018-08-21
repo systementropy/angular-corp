8d5b5c11a68b4429bf47e92f3f483313a2f77379
systementropy <vatsa.saket@gmail.com>
2018-08-21 16:11:40 +0530

    he;

--- app/script/career-main.js
+++ app/script/career-main.js
@@ -13,6 +13,18 @@
     ])
 
     .controller('CareerMainCtrl', ['$rootScope' ,'$scope', '$http','$timeout','$location','parallaxHelper','$window', function($rootScope,$scope, $http,$timeout,$location,parallaxHelper,$window) {
+        $scope.updShowAll = function(){
+            $scope.jobs=$scope.eachJob;
+            $scope.validParent={};
+            for(var i=0; i<$scope.jobs.length; i++){
+                var thisJob=$scope.jobs[i];
+                if (!$scope.validParent[thisJob['Parent']]) {
+                    $scope.validParent[thisJob['Parent']]=thisJob['Function'];
+                }
+            }
+            console.log($scope.validParent);
+            $scope.showAll = true;
+        }
         $scope.init = function(){
             $rootScope.hideHeader=false;
             $rootScope.hidefooter=false;
@@ -20,19 +32,25 @@
             if ($scope.url!=undefined) {
                 $scope.url_id = $scope.url.split('=')[1];
             }else {
-                $scope.url_id='featured';
+                $scope.url_id='all';
             }
 
             $http.get('/script/career.json')
             .then(function(response) {
                 $scope.eachJob=response.data;
+                console.log($scope.eachJob);
                 $scope.jobs=[];
-                for(var i=0; i<$scope.eachJob.length; i++){
-                    if($scope.eachJob[i]['Parent']==$scope.url_id){
-
-                        $scope.jobs.push($scope.eachJob[i]);
-                    }
+                if ($scope.url_id=='all') {
+                    $scope.jobs=$scope.eachJob;
+                    $scope.showAll = true;
+                }else{
+                    for(var i=0; i<$scope.eachJob.length; i++){
+                        if($scope.eachJob[i]['Parent']==$scope.url_id){
+                            $scope.jobs.push($scope.eachJob[i]);
+                        }
+                    }    
                 }
+                
 
             }, function(response) {
                 console.log('Error'+response);
